3e5cfe6c5742303668d07137b20751ba9ad0cfcd
Johnny Wu <KarasAya@gmail.com>
2013-05-20 18:44:39 +0800

    update css parser

--- builtin/modules/ui/core/css.lua
+++ builtin/modules/ui/core/css.lua
@@ -37,9 +37,9 @@ local __style = nil
 -- local
 --/////////////////////////////////////////////////////////////////////////////
 
-local def = function ( _prop_name, _parse_func )
+local def = function ( _prop_name, _parse_func, _options )
     __M[_prop_name] = function ( _text )
-        __style[_prop_name] = _parse_func (_text)
+        _parse_func ( __style, _prop_name, _text, _options )
         return __M
     end
 end
@@ -48,28 +48,109 @@ end
 -- Desc: 
 -- ------------------------------------------------------------------ 
 
-local parse_option = function ( _text, _options )
+local parse_option = function ( _style, _prop_name, _text, _options )
+    local valid_option = false
+    for i=1,#_options do 
+        if _options[i] == _text then
+            valid_option = true
+            break
+        end
+    end
+
+    assert ( valid_option, "Can't find the option " .. _text )
+    _style[_prop_name] = _text
 end
 
 -- ------------------------------------------------------------------ 
 -- Desc: 
 -- ------------------------------------------------------------------ 
 
-local parse_offset_rect = function ( _text )
+local parse_size = function ( _style, _prop_name, _text, _options )
+    if _text == "inherit" then
+        _style[_prop_name] = { "inherit" }
+        return
+    elseif _text == "auto" then
+        _style[_prop_name] = { "auto" }
+        return
+    end
+
+    local len = _text:len()
+    if _text:ncmp("px", len-2, 2) then
+        _style[_prop_name] = { "px", tonumber( _text:sub( 1, len-2 ) )  }
+    elseif _text:ncmp("%", len-1, 1) then
+        _style[_prop_name] = { "%", tonumber( _text:sub( 1, len-1 ) )  }
+    end
 end
 
 -- ------------------------------------------------------------------ 
 -- Desc: 
 -- ------------------------------------------------------------------ 
 
-local parse_value = function ( _text )
+local parse_color = function ( _style, _prop_name, _text, _options )
+    -- #f00, #ff0000
+    if _text:ncmp("#",0,1) then
+        local v = _text:sub(2)
+        if #v == 3 then
+            local r = _text:sub(2,2)
+            local g = _text:sub(3,3)
+            local b = _text:sub(4,4)
+            _style[_prop_name] = { tonumber(r..r,16), tonumber(g..g,16), tonumber(b..b,16), 255 }
+        elseif #v == 6 then
+            local r = _text:sub(2,3)
+            local g = _text:sub(4,5)
+            local b = _text:sub(6,7)
+            _style[_prop_name] = { tonumber(r,16), tonumber(g,16), tonumber(b,16), 255 }
+        else
+            error ( "invalid color value " .. _text )
+        end
+
+    -- rgb or rgba
+    elseif _text:ncmp("rgb",0,3) then
+        local list = {}
+        for w in string.gmatch(_text, "%d+") do
+            table.add ( list, w )
+        end
+        local r = list[1] or 255
+        local g = list[2] or 255
+        local b = list[3] or 255
+        local a = list[4] or 255
+        _style[_prop_name] = { r, g, b, a }
+    end
 end
 
 -- ------------------------------------------------------------------ 
 -- Desc: 
 -- ------------------------------------------------------------------ 
 
-local parse_color = function ( _text )
+local parse_offset_rect = function ( _style, _prop_name, _text, _options )
+    local list = {}
+    for w in string.gmatch(_text, "%S+") do
+        table.add ( list, w )
+    end
+    if #list == 0 then return end
+
+    local top = list[1]
+    local right = list[2] or top
+    local bottom = list[3] or top
+    local left = (list[4] or right) or top
+
+    parse_size( _style, _prop_name .. "_top", top, _options )
+    parse_size( _style, _prop_name .. "_right", right, _options )
+    parse_size( _style, _prop_name .. "_bottom", bottom, _options )
+    parse_size( _style, _prop_name .. "_left", left, _options )
+end
+
+-- ------------------------------------------------------------------ 
+-- Desc: 
+-- ------------------------------------------------------------------ 
+
+local parse_font_family = function ( _style, _prop_name, _text, _options )
+    local list = {}
+    for w in string.gmatch(_text, "[^,]+") do
+        table.add ( list, w )
+    end
+    if #list == 0 then return end
+    _style[_prop_name] = list
 end
 
 --/////////////////////////////////////////////////////////////////////////////
@@ -104,24 +185,31 @@ __M.done = done
 -- Desc: 
 -- ------------------------------------------------------------------ 
 
+-- def ( "font",  parse_font )
+def ( "font_family",  parse_font_family )
+def ( "font_size",  parse_size )
+def ( "font_style",  parse_option, { "normal", "italic", "oblique", "inherit" } )
+
 def ( "margin",  parse_offset_rect )
-def ( "margin_left",    parse_value )
-def ( "margin_right",   parse_value )
-def ( "margin_top",     parse_value )
-def ( "margin_bottom",  parse_value )
+def ( "margin_left",    parse_size )
+def ( "margin_right",   parse_size )
+def ( "margin_top",     parse_size )
+def ( "margin_bottom",  parse_size )
 
 def ( "padding",  parse_offset_rect )
-def ( "padding_left",    parse_value )
-def ( "padding_right",   parse_value )
-def ( "padding_top",     parse_value )
-def ( "padding_bottom",  parse_value )
+def ( "padding_left",    parse_size )
+def ( "padding_right",   parse_size )
+def ( "padding_top",     parse_size )
+def ( "padding_bottom",  parse_size )
 
 def ( "border",  parse_offset_rect )
-def ( "border_left",    parse_value )
-def ( "border_right",   parse_value )
-def ( "border_top",     parse_value )
-def ( "border_bottom",  parse_value )
+def ( "border_left",    parse_size )
+def ( "border_right",   parse_size )
+def ( "border_top",     parse_size )
+def ( "border_bottom",  parse_size )
 def ( "border_color",  parse_color )
+
+def ( "color",  parse_color )
 
 --/////////////////////////////////////////////////////////////////////////////
 --
--- examples/test_app/test_css.lua
+++ examples/test_app/test_css.lua
@@ -17,11 +17,18 @@ return {
 
     init = function ( _root_el ) 
         local style = ui.css.build()
-                            .font_size("20px")
-                            .text_outline("1px #000")
-                            .color("#0f0")
                             .margin("0px 0px 0px 10px")
+                            -- .border("20px 10px")
+                            -- .padding("auto")
+                            .color("rgb( 255, 0, 0) ")
                             .done()
+
+        -- local style = ui.css.build()
+        --                     .font_size("20px")
+        --                     .text_outline("1px #000")
+        --                     .color("#0f0")
+        --                     .margin("0px 0px 0px 10px")
+        --                     .done()
 
         debug.dump(style)
     end,
