7c3417d1cd1b9158eb43bda2775161415ac63dc8
Wu Jie <KarasAya@gmail.com>
2014-02-16 23:27:42 +0800

    changes

--- core/lua/wrap_font.c
+++ core/lua/wrap_font.c
@@ -242,14 +242,14 @@ static int __lua_font_wrap_text ( lua_State *_l ) {
     const char *text, *whitespace;
     int maxWidth;
 
-    const char *str, *laststr, *pword;
+    const char *str, *laststr, *nextstr, *pword;
     char *newtext, *newtext_p, *last_newtext;
     int ch, next_ch, len, newlen;
     uint ft_index, prev_ft_index;
     int cur_x, last_x, word_x, advance;
     ex_glyph_t *glyph;
     bool linebreak, beginningOfLine, trimWhitespace; 
-    bool wrapword, collapseSpace, collapseLinebreak, needwrap;
+    bool wrapword, collapseSpace, collapseLinebreak;
 
     // get lua arguments
     ex_lua_check_nargs(_l,5);
@@ -262,12 +262,12 @@ static int __lua_font_wrap_text ( lua_State *_l ) {
 
     //
     len = strlen(text);
-    str = laststr = pword = text;
+    str = laststr = nextstr = pword = text;
     newtext_p = newtext = last_newtext = ex_malloc( len * sizeof(char) );
     prev_ft_index = -1;
 
     // get wrapMode
-    needwrap = wrapword = false;
+    wrapword = false;
     collapseSpace = collapseLinebreak = false;
     if ( !strncmp( whitespace, "pre-wrap", 8 ) ) {
         wrapword = true;
@@ -299,8 +299,7 @@ static int __lua_font_wrap_text ( lua_State *_l ) {
     cur_x = word_x = 0;
     linebreak = false;
     while ( *str ) {
-        str += utf8proc_iterate ((const uint8_t *)str, -1, &ch);
-        advance = 0;
+        nextstr += utf8proc_iterate ((const uint8_t *)str, -1, &ch);
 
         // if this is line-break
         if ( ch == '\n' || ch == '\r' ) {
@@ -315,86 +314,136 @@ static int __lua_font_wrap_text ( lua_State *_l ) {
         // if this is space 
         if ( ch == ' ' || ch == '\t' || ch == '\f' ) {
             if ( collapseSpace ) {
-                ch = ' '; // yes, must turn it to space to make sure only one space
-                utf8proc_iterate ((const uint8_t *)str, -1, &next_ch);
+                const char * nextnextstr = nextstr;
+                while ( *nextstr ) {
+                    nextstr = nextnextstr;
+                    nextnextstr += utf8proc_iterate ((const uint8_t *)nextstr, -1, &next_ch);
 
-                // if next_ch is white-space, then collapse this char
-                if ( next_ch == ' ' || next_ch == '\t' || next_ch == '\f' ) {
-                    laststr = str;
-                    continue;
-                }
-
-                // if next_ch is line-break and collapseLinebreak is true, then collapse this char
-                if ( collapseLinebreak ) {
-                    if ( next_ch == '\n' || next_ch == '\r' ) {
-                        laststr = str;
+                    // if next_ch is white-space, then collapse this char
+                    if ( next_ch == ' ' || next_ch == '\t' || next_ch == '\f' ) {
+                        str = nextstr;
                         continue;
                     }
+
+                    // if next_ch is line-break and collapseLinebreak is true, then collapse this char
+                    if ( next_ch == '\n' || next_ch == '\r' ) {
+                        if ( collapseLinebreak ) {
+                            str = nextstr;
+                            continue;
+                        }
+                    }
+
+                    //
+                    break;
                 }
 
                 // skip first-time collapse
                 if ( trimWhitespace ) {
                     trimWhitespace = false;
-                    laststr = str;
+                    str = nextstr;
                     continue;
                 }
+
+                // yes, must turn it to space to make sure only one space
+                ch = ' ';
             }
         }
 
-        // TODO: if this is a word
-        // process last word
-        if ( ch == ' ' || ch == '\t' || ch == '\f' || ch == '\n' || ch == '\r' ) {
-            beginningOfLine = false;
-
-            // ERROR TODO!!! { 
-            // NOTE: this flag will control to skip current white-space and add it to text2 when word-wrap already happens, 
-            // if ( needwrap ) {
-            //     word_x = last_x;
-            //     pword = laststr;
-            //     last_newtext = newtext_p;
-            // }
-            // else {
-            //     word_x = cur_x;
-            //     pword = str;
-            //     last_newtext = newtext_p + (str-laststr);
-            // }
-            // } TODO end 
-            word_x = last_x;
-            pword = laststr;
-            last_newtext = newtext_p;
-        }
+        //
         trimWhitespace = false;
 
-        ft_index = ex_font_get_index ( font, ch );
-        glyph = ex_font_get_glyph ( font, ft_index );
-        advance += ex_font_get_kerning( font, prev_ft_index, ft_index );
-        advance += glyph->advance_x;
+        // process word-break, word-wrap
+        if ( wrapword ) {
+            pword = str;
+            word_x = cur_x;
 
-        // check if wrap to new-line
-        if ( wrapword && cur_x + advance > maxWidth ) {
-            needwrap = true;
-            if ( !beginningOfLine ) {
-                linebreak = true;
+            next_ch = ch;
+            const char * nextnextstr = nextstr;
+            while (1) {
+                bool canbreak = false;
 
-                str = pword;
-                cur_x = word_x;
-                newtext_p = last_newtext;
-                break;
+                // if this character can break
+                if ( next_ch == ' ' || next_ch == '\t' || next_ch == '\f' || next_ch == '\n' || next_ch == '\r' ) {
+                    pword = nextstr;
+                    word_x = cur_x;
+
+                    beginningOfLine = false;
+                    canbreak = true;
+                }
+
+                // advanced character
+                advance = 0;
+                ft_index = ex_font_get_index ( font, next_ch );
+                glyph = ex_font_get_glyph ( font, ft_index );
+                advance += ex_font_get_kerning( font, prev_ft_index, ft_index );
+                advance += glyph->advance_x;
+                cur_x += advance;
+                prev_ft_index = ft_index;
+
+                //
+                if ( canbreak ) {
+                    if ( cur_x > maxWidth ) {
+                        if ( !beginningOfLine ) {
+                            linebreak = true;
+
+                            nextstr = pword;
+                            cur_x = word_x;
+                        }
+                    }
+                    break;
+                }
+
+                //
+                nextstr = nextnextstr;
+                nextnextstr += utf8proc_iterate ((const uint8_t *)nextstr, -1, &next_ch);
+
+                //
+                if ( *nextnextstr == NULL )
+                    break;
             }
         }
+        else {
+            // advanced character
+            advance = 0;
+            ft_index = ex_font_get_index ( font, ch );
+            glyph = ex_font_get_glyph ( font, ft_index );
+            advance += ex_font_get_kerning( font, prev_ft_index, ft_index );
+            advance += glyph->advance_x;
+            cur_x += advance;
+            prev_ft_index = ft_index;
+        } 
 
-        // advanced character
-        last_x = cur_x;
-        cur_x += advance;
-        prev_ft_index = ft_index;
+        // DELME { 
+        // TODO: word step, like above, we should step word here. 
+        // process last word
+        // if ( ch == ' ' || ch == '\t' || ch == '\f' || ch == '\n' || ch == '\r' ) {
+        //     beginningOfLine = false;
+
+        //     // ERROR TODO!!! { 
+        //     // NOTE: this flag will control to skip current white-space and add it to text2 when word-wrap already happens, 
+        //     // if ( needwrap ) {
+        //     //     word_x = last_x;
+        //     //     pword = laststr;
+        //     //     last_newtext = newtext_p;
+        //     // }
+        //     // else {
+        //     //     word_x = cur_x;
+        //     //     pword = str;
+        //     //     last_newtext = newtext_p + (str-laststr);
+        //     // }
+        //     // } TODO end 
+        //     word_x = last_x;
+        //     pword = nextstr;
+        //     last_newtext = newtext_p;
+        // }
+        // } DELME end 
 
         // copy character to newtext_p
-        strncpy( newtext_p, laststr, str - laststr );
-        newtext_p += (str - laststr);
-        laststr = str;
+        strncpy( newtext_p, str, nextstr - str );
+        newtext_p += (nextstr - str);
+        str = nextstr;
 
         if ( linebreak ) {
-            needwrap = false;
             break;
         }
     }
